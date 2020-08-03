29b7f7dc9267ffe1b47b652ca5aa7f446146ba30
chinotrb <chinotrb@gmail.com>
2020-08-03 11:40:21 -0600

    css , jv , html - block Notes

---
+++ list.js
@@ -0,0 +1,53 @@
+$(document).ready(inicializarEventos);
+
+function inicializarEventos() {
+  $("#add").click(agregarLista);
+  $("#Remove").click(borar);
+  $("#RemodeAll").click(borarTodo);
+  $("#jumbotron").dblclick(clic);
+
+ recognizeKeyboard()
+
+}
+/*
+function agregarLista() {
+
+  $("ul").append("<li>" + $("#agregarobjeto").val() );
+  $("#agregarobjeto").val("");
+
+}*/
+
+function agregarLista() {
+
+  $("ul").append(" <input type=checkbox>" + $("#agregarobjeto").val() + "<br>" );
+  $("#agregarobjeto").val("");
+
+}
+
+function borarTodo(){
+  let x = $("ul")
+  x.empty();
+}
+
+function borar() {
+  let x = $(" li" );
+  x = x.eq(-1);
+  x.remove();
+}   
+ 
+function clic(){
+  let x = $(" li" );
+  x.remove()
+}
+
+
+  function recognizeKeyboard() {
+    document.addEventListener('keydown', function (tecla) {
+  
+      if (tecla.keyCode == 13) {
+        agregarLista()
+        console.log("se a agrgado algo")
+    }
+  
+    })}
+
